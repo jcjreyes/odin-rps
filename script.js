cfc5e0fc19655cc1a64ff663c07986e9c9068024
Justin <justinreyes13003@gmail.com>
2022-02-12 20:06:09 +0800

    integrated DOM, 90% js

--- script.js
+++ script.js
@@ -1,12 +1,64 @@
-let choices = ["rock", "paper", "scissors"];
+
 
 // this function makes the computer choose a random option from choices
 // anonymous function allows the choice to instantiate the computer's move
+// let computerPlay = () => {
+//     return choices[Math.floor(Math.random()*choices.length)];
+// };
+
+// function game() {
+
+//     let playerScore = 0;
+//     let computerScore = 0;
+//     while (true) {
+//         let playerSelection = prompt("Enter your choice");
+//         computerSelection = computerPlay();
+//         let roundResult = playRound(playerSelection, computerSelection);
+//         if (playerSelection == computerSelection) {
+//             console.log("Tie. Try again");
+//         } else if (roundResult == true) {
+//             playerScore++;
+//             console.log(`${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`);
+//         } else {
+//             computerScore++;
+//             console.log(`${capitalize(playerSelection)} is beaten by ${capitalize(computerSelection)}`);
+//         }
+//         console.log(`Current score: ${playerScore} - ${computerScore}`)
+//         if (playerScore == 5 || computerScore == 5) {
+//             break;
+//         }
+//     };
+
+//     if (playerScore > computerScore) {
+//         console.log("CONGRATULATIONS! You win");
+//     } else {
+//         console.log("Sorry. You lost.");
+//     }
+// }
+
+let choices = ["rock", "paper", "scissors"];
+const rockBtn = document.querySelector('.rock');
+const paperBtn = document.querySelector('.paper');
+const scissorsBtn = document.querySelector('.scissors');
+const buttons = [rockBtn, paperBtn, scissorsBtn];
+const gameStatus = document.querySelector('.log');
+
+const playerScore = document.querySelector('.player');
+const pcScore = document.querySelector('.computer');
+
+let playerChoice;
+let computerChoice;
+let playing = true;
+
 let computerPlay = () => {
     return choices[Math.floor(Math.random()*choices.length)];
 };
 
-function playRound(playerSelection, computerSelection) {
+const capitalize = (string) => {
+    return string.at(0).toUpperCase() + string.substring(1);
+}
+
+const playRound = (playerSelection, computerSelection) => {
     playerSelection = playerSelection.toLowerCase();
     if (playerSelection == computerSelection) {
         return false;
@@ -33,36 +85,21 @@ function playRound(playerSelection, computerSelection) {
     }
 };
 
-function game() {
-
-    function capitalize(string) {
-        return string.at(0).toUpperCase() + string.substring(1);
-    }
-
-    let playerScore = 0;
-    let computerScore = 0;
-    while (true) {
-        let playerSelection = prompt("Enter your choice");
-        computerSelection = computerPlay();
-        let roundResult = playRound(playerSelection, computerSelection);
-        if (playerSelection == computerSelection) {
-            console.log("Tie. Try again");
-        } else if (roundResult == true) {
-            playerScore++;
-            console.log(`${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`);
-        } else {
-            computerScore++;
-            console.log(`${capitalize(playerSelection)} is beaten by ${capitalize(computerSelection)}`);
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        playerChoice = e.target.innerText;
+        computerChoice = computerPlay();
+        if (playing) {
+            if (playerScore.textContent == 5 || pcScore.textContent == 5) {
+                gameStatus.textContent = `${(playerScore.textContent == 5) ? 'player' : 'computer'} wins!`
+                playing = false;
+            } else if (playRound(playerChoice, computerChoice) == true) {
+                gameStatus.textContent = `${playerChoice} beats ${capitalize(computerChoice)}`;
+                playerScore.textContent = Number(playerScore.textContent) + 1;
+            } else {
+                gameStatus.textContent = `${playerChoice} is beaten by ${capitalize(computerChoice)}`;
+                pcScore.textContent = Number(pcScore.textContent) + 1;
+            }
         }
-        console.log(`Current score: ${playerScore} - ${computerScore}`)
-        if (playerScore == 5 || computerScore == 5) {
-            break;
-        }
-    };
-
-    if (playerScore > computerScore) {
-        console.log("CONGRATULATIONS! You win");
-    } else {
-        console.log("Sorry. You lost.");
-    }
-}
+    })
+})
